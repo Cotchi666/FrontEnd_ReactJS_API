4bc88470a10984ce374aec5804e03e5cc146f844
chien nguyen <chiennguyen.170901@gmail.com>
2022-06-22 22:40:32 +0700

    update UI

--- src/containers/App.js
+++ src/containers/App.js
@@ -18,7 +18,7 @@ import System from '../routes/System';
 import { CustomToastCloseButton } from '../components/CustomToast';
 import ConfirmModal from '../components/ConfirmModal';
 import HomePage from './HomePage/HomeHeader.js';
-
+import CustomScrollbars from '../components/CustomScrollbars'
 class App extends Component {
 
     handlePersistorState = () => {
@@ -44,17 +44,18 @@ class App extends Component {
             <Fragment>
                 <Router history={history}>
                     <div className="main-container">
-                        <ConfirmModal />
                         {this.props.isLoggedIn && <Header />}
 
-                        <span className="content-container">
+                        <div className="content-container">
+                            <CustomScrollbars style= {{height :'100vh', width :'100%'}}>
                             <Switch>
                                 <Route path={path.HOME} exact component={(Home)} />
                                 <Route path={path.LOGIN} component={userIsNotAuthenticated(Login)} />
                                 <Route path={path.SYSTEM} component={userIsAuthenticated(System)} />
                                 <Route path={path.HOMEPAGE} component={HomePage} />
                             </Switch>
-                        </span>
+                            </CustomScrollbars>
+                        </div>
 
                         <ToastContainer
                             className="toast-container" toastClassName="toast-item" bodyClassName="toast-item-body"
