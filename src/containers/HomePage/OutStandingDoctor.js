0fcd723f668fd5c0ec01425c409045d25166a22d
chien nguyen <chiennguyen.170901@gmail.com>
2022-06-23 18:47:43 +0700

    update UI

---
+++ src/containers/HomePage/OutStandingDoctor.js
@@ -0,0 +1,59 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import './MedicalFacility.scss';
+import Slider from 'react-slick';
+
+
+class Specialty extends Component {
+
+
+    render() {
+
+        return (
+            <div className="section-share section-outstanding-doctor">
+                <div className="section-container">
+                    <div className="section-header">
+
+                        <span className="title-section">Bác sĩ nổi bật tuần qua</span>
+
+                        <button className="btn-section"> Xem thêm </button>
+
+                    </div>
+                    <div className="section-body">
+                        <Slider {...this.props.settings}>
+
+                            <div className="section-customize">
+                                <div className="customize-border">
+                                    <div className="outer-bg">                           
+                                     <div className="bg-image  section-outstanding-doctor" />
+                                    </div>
+                                    <div className="position text-center">
+                                        <div> Giáo sư, Tiến Sĩ Chiến Nguyễn</div>
+                                        <div >Cơ xương khớp</div>
+                                    </div>
+                                   
+                                </div>
+
+                            </div>
+                            
+                        </Slider>
+                    </div>
+
+                </div>
+
+            </div>
+        )
+    }
+}
+const mapStateToProps = (state) => {
+    return {
+        isLoggedIn: state.user.isLoggedIn,
+        language: state.app.language,
+    };
+};
+const mapDispatchToProps = dispatch => {
+    return {
+        // changeLanguageAppRedux: (language) => dispatch(changeLanguageApp(language))
+    };
+};
+export default connect(mapStateToProps, mapDispatchToProps)(Specialty);
--- src/containers/HomePage/Specialty.js
+++ src/containers/HomePage/Specialty.js
@@ -10,48 +10,42 @@ class Specialty extends Component{
 
 
     render(){
-        let settings ={
-            dots:false,
-            infinite:true,
-            speed:500,
-            slidesToShow:4,
-            slidesToScroll:1
-        }
+       
         return(
-            <div className="section-specialty">
-                <div className="specialty-container">
-                    <div className="specialty-header">
+            <div className="section-share section-specialty">
+                <div className="section-container">
+                    <div className="section-header">
                         <span className="title-section"> Chuyên khoa phổ biến</span>
                         <button  className="btn-section"> Xem thêm</button>
                     </div>
-                    <div className="specialty-body">
-                    <Slider {...settings}>
+                    <div className="section-body">
+                    <Slider {...this.props.settings}>
                         
-                        <div className="img-customize">
-                            <div className="bg-image"/>
+                        <div className="section-customize">
+                            <div className="bg-image section-specialty section-specialty"/>
                             <div >Cơ xương khớp 1</div>
 
                         </div>
                         <div className="img-customize">
-                            <div className="bg-image"/>
+                            <div className="bg-image section-specialty"/>
                             <div >Cơ xương khớp 2</div>
 
                         </div> <div className="img-customize">
-                            <div className="bg-image"/>
+                            <div className="bg-image section-specialty"/>
                             <div >Cơ xương khớp 3</div>
 
                         </div> <div className="img-customize">
-                            <div className="bg-image"/>
+                            <div className="bg-image section-specialty"/>
                             <div >Cơ xương khớp 4</div>
 
                         </div> <div className="img-customize">
-                            <div className="bg-image"/>
+                            <div className="bg-image section-specialty"/>
                             <div >Cơ xương khớp 5</div>
 
                         </div>
 
                         <div className="img-customize">
-                            <div className="bg-image"/>
+                            <div className="bg-image section-specialty"/>
                             <div >Cơ xương khớp 6</div>
 
                         </div>
