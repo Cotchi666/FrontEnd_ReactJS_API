50c162dbdfb3ee8913bd963e8bd296547656628e
CotChi666 <chiennguyen.170901@gmail.com>
2022-06-28 15:44:13 +0700

    update

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 1. Download nodejs ver 16 or higher.
-2. Download nvm for nodejs (package manager).
+2. Download nvm for nodejs (package manager). (--legacy-peer-deps)
 3. clone the project into your device.
 4. run "npm install" on ur cmd of the project that cloned.
 5. run "npm start" for the first deploying on ur locall server.
--- src/containers/System/Admin/UserRedux.js
+++ src/containers/System/Admin/UserRedux.js
@@ -18,8 +18,62 @@ class UserRedux extends Component {
                     User redux chien
                 </div>
                 <div className="user-redux-body">
-                    <div >
-                        thêm mới người dùng
+                    <div className="container" >
+                        <div className="row">
+
+                            <div className="col-12"><FormattedMessage id ="manage-user.add"/></div>
+                            <div className="col-3">
+                                <label> <FormattedMessage id ="manage-user.email"/></label>
+                                <input className="form-control" type="email" />
+                            </div>
+                            <div className="col-3">
+                                <label> <FormattedMessage id ="manage-user.password"/></label>
+                                <input className="form-control" type="password" />
+                            </div>
+                            <div className="col-3">
+                                <label> <FormattedMessage id ="manage-user.first-name"/> </label>
+                                <input className="form-control" type="text" />
+                            </div>
+                            <div className="col-3">
+                                <label> <FormattedMessage id ="manage-user.last-name"/></label>
+                                <input className="form-control" type="text" />
+                            </div>
+                            <div className="col-3">
+                                <label> <FormattedMessage id ="manage-user.phone-number"/> </label>
+                                <input className="form-control" type="text" />
+                            </div>
+                            <div className="col-3">
+                                <label> <FormattedMessage id ="manage-user.address"/> </label>
+                                <input className="form-control" type="text" />
+                            </div>
+                            <div className="col-3">
+                                <label> <FormattedMessage id ="manage-user.position"/> </label>
+                                <select className="form-control" >
+                                    <option selected> Choose</option>
+                                    <option > ...</option>
+
+                                </select>
+                            </div>
+                            <div className="col-3">
+                                <label>  <FormattedMessage id ="manage-user.role"/> </label>
+                                <select className="form-control" >
+                                    <option selected> Choose...</option>
+                                    <option > ...</option>
+
+                                </select>
+                            </div>
+                            <div className="col-3">
+                                <label>  <FormattedMessage id ="manage-user.image"/> </label>
+                                <input className="form-control" type="text" />
+                            </div>
+                            <div className="col-12">   
+                            <button className="btn btn-primary">
+                            <FormattedMessage id ="manage-user.save"/>
+                             </button>
+                             </div>
+
+
+                        </div>
                     </div>
                 </div>
             </div>
