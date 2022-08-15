c69e3075d77864e983bce395c012f2391ba8c336
Nguyễn Ngọc Chiến <56423606+Cotchi666@users.noreply.github.com>
2022-08-15 16:55:40 +0700

    update

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
-import { Nav, Container, Navbar, Badge } from "react-bootstrap";
+import { Nav, Container, Navbar, Badge, NavDropdown } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
 import { Store } from "./Store";
 import { useContext } from "react";
@@ -10,7 +10,12 @@ import CartScreen from "./screens/CartScreen";
 import SigninScreen from "./screens/SigninScreen";
 function App() {
   const { state, dispatch: ctxDispatch } = useContext(Store);
-  const { cart } = state;
+  const { cart, userInfo } = state;
+  const signoutHandle = () => {
+    ctxDispatch({ type: "USER_SIGNOUT" });
+    localStorage.removeItem("userInfo");
+  };
+  
   return (
     <BrowserRouter>
       <div className="d-flex flex-column site-container">
@@ -25,10 +30,35 @@ function App() {
                   Cart
                   {cart.cartItems.length > 0 && (
                     <Badge pill bg="danger">
-                      {cart.cartItems.reduce((a,c)=> a+c.quantity,0)}
+                      {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
                     </Badge>
                   )}
                 </Link>
+                {userInfo ? (
+                  <NavDropdown
+                    title={userInfo.username}
+                    id="basic-nav-dropdown"
+                  >
+                    <LinkContainer to="/profile">
+                      <NavDropdown.Item>User Profile</NavDropdown.Item>
+                    </LinkContainer>
+                    <LinkContainer to="/orderhistory">
+                      <NavDropdown.Item>Order History</NavDropdown.Item>
+                    </LinkContainer>
+                    <NavDropdown.Divider />
+                    <Link
+                      className="dropdown-item"
+                      to="#signup"
+                      onClick={signoutHandle}
+                    >
+                      Sign Out
+                    </Link>
+                  </NavDropdown>
+                ) : (
+                  <Link className="nav-link" to="/signin">
+                    Sign In
+                  </Link>
+                )}
               </Nav>
             </Container>
           </Navbar>
@@ -41,8 +71,8 @@ function App() {
                 path="/classes/Room/:objectId"
                 element={<ProductScreen />}
               />
-              <Route path="/cart" element={<CartScreen/>} />
-              <Route path="/signin" element={<SigninScreen/>} />
+              <Route path="/cart" element={<CartScreen />} />
+              <Route path="/signin" element={<SigninScreen />} />
             </Routes>
           </Container>
         </main>
--- src/screens/SigninScreen.js
+++ src/screens/SigninScreen.js
@@ -1,27 +1,31 @@
 import { Button, Container, Form } from "react-bootstrap";
-import { useLocation, Link } from "react-router-dom";
+import { useLocation, Link, useNavigate } from "react-router-dom";
 import { Helmet } from "react-helmet-async";
 import userAPI from "../api/userAPI";
 import { useContext, useState } from "react";
 import { Store } from "../Store";
 
 export default function SigninScreen() {
+  const navigate = useNavigate();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const { search } = useLocation();
   const redirectInUrl = new URLSearchParams(search).get("redirect");
   const redirect = redirectInUrl ? redirectInUrl : "/";
 
-  const { state, dispatch: ctxDispatch } = useContext(Store); 
+  const { state, dispatch: ctxDispatch } = useContext(Store);
 
   const submitHandler = async (e) => {
     e.preventDefault();
     try {
-      const data  = await userAPI.login(email, password);
-      console.log("check data ", data)
-      ctxDispatch({type : 'USER_SIGNIN', payload: data})
-      
-    } catch (error) {}
+      const data = await userAPI.login(email, password);
+      console.log("check data user ", data.result);
+      ctxDispatch({ type: "USER_SIGNIN", payload: data.result });
+      localStorage.setItem("userInfo", JSON.stringify(data.result));
+      navigate(redirect || "/");
+    } catch (error) {
+      alert("Invalid email or password");
+    }
   };
   return (
     <Container className="small-container">
